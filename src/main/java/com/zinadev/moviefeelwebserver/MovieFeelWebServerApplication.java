8bb08a5c30fcc078f877df532b3a6b6fa58ff603
sebi1995 <s.zdroana@gmail.com>
2022-03-31 16:21:33 +0300

    initialised project

---
+++ src/main/java/com/zinadev/moviefeelwebserver/MovieFeelWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.zinadev.moviefeelwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFeelWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFeelWebServerApplication.class, args);
+	}
+
+}
